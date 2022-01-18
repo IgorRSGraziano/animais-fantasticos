f8b3cd7565d0be82a932c2a85dd69494d2d0241d
IgorRSGraziano <igorrsgraziano@gmail.com>
2022-01-18 20:33:00 -0300

    change js class to data's

--- modules/script.js
+++ modules/script.js
@@ -1,6 +1,8 @@
+//Funções e variáveis
+
 function initTabMenu() {
-    const tabMenu = document.querySelectorAll('.js-tabmenu li')
-    const tabContent = document.querySelectorAll('.js-tabmenu-content section')
+    const tabMenu = document.querySelectorAll('[data-animals="list"] li')
+    const tabContent = document.querySelectorAll('[data-tab="menu"] section')
     tabContent[0].classList.add('ativo')
 
     //Verifica se os itens existem para realizar a manipulação
@@ -9,7 +11,8 @@ function initTabMenu() {
             tabContent.forEach((section) => {
                 section.classList.remove('ativo')
             })
-            tabContent[index].classList.add('ativo')
+            const data = tabContent[index].dataset.anime
+            tabContent[index].classList.add('ativo', data)
         }
 
         tabMenu.forEach((itemMenu, index) => {
@@ -20,10 +23,8 @@ function initTabMenu() {
     }
 }
 
-initTabMenu()
-
 function initAccordion() {
-    const accordionList = document.querySelectorAll('.js-accordion dt')
+    const accordionList = document.querySelectorAll('[data-effect="accordion"] dt')
 
     function activeAccordion() {
         this.classList.toggle('ativo')
@@ -35,13 +36,10 @@ function initAccordion() {
     })
 }
 
-initAccordion()
-
-
 //Após clicar em algum link interno no menu, irá realizar um scroll para o local em que ele foi designado de forma suave
 function initSmoothScroll() {
-    //Seleciona todos elementos 'a' do '.js-menu' que o href comece com '#'
-    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+    //Seleciona todos elementos 'a' do '[data-menu='smooth']' que o href comece com '#'
+    const linksInternos = document.querySelectorAll('[data-menu="smooth"] a[href^="#"]')
     function scrollToSection(event) {
         event.preventDefault()
         const href = event.currentTarget.getAttribute('href')
@@ -57,11 +55,9 @@ function initSmoothScroll() {
     })
 }
 
-initSmoothScroll()
-
 //Gera a animação de itens "aparecendo" no site no decorrer que a pessoa vai descendo a tela
 function initAnimacaoScroll() {
-    const section = document.querySelectorAll('.js-scroll')
+    const section = document.querySelectorAll('[data-anime="scroll"]')
     if (section.length) {
         const windowHeight = window.innerHeight * 0.6
 
@@ -79,4 +75,26 @@ function initAnimacaoScroll() {
     }
 }
 
+
+//Execução
 initAnimacaoScroll()
+initSmoothScroll()
+initAccordion()
+initTabMenu()
+
+//Exercicios
+
+// Adicione um atributo data-anime="show-down" e
+// data-anime="show-right" a todos as section's
+// com descricão dos animais.
+
+
+// Utilizando estes atributos, adicione a classe
+// show-down ou show-right a sua respectiva section
+// assim que a mesma aparecer na tela (animacao tab)
+
+// No CSS faça com que show-down anime de cima para baixo
+// e show-right continue com a mesma animação da esquerda
+// para a direita
+
+// Substitua todas as classes js- por data atributes.
