b91de0f0be690952f1ab80064b6de23fc4074339
IgorRSGraziano <igorrsgraziano@gmail.com>
2022-02-02 17:26:03 -0300

    feat: add modal section after click in "login"

--- modules/script.js
+++ modules/script.js
@@ -2,8 +2,10 @@ import smoothScroll from './smoothScroll.js';
 import animationScroll from './animationScroll.js'
 import accordion from './accordion.js'
 import tabMenu from './tabMenu.js'
+import initModal from './modal.js'
 
 tabMenu()
 animationScroll()
 smoothScroll()
 accordion()
+initModal()
